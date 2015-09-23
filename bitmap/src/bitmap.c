1fed70f6b8da947143c90b352cc9982fc379b9f0
Will Starms <vilhelmen@gmail.com>
2015-09-23 16:27:58 -0500

    Minor change of no importance

--- bitmap/src/bitmap.c
+++ bitmap/src/bitmap.c
@@ -39,7 +39,7 @@ const static uint8_t invert_mask[8] = { 0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF
 #define B2(n) n,     n+1,     n+1,     n+2
 #define B4(n) B2(n), B2(n+1), B2(n+1), B2(n+2)
 #define B6(n) B4(n), B4(n+1), B4(n+1), B4(n+2)
-static const uint8_t bit_totals[256] = { B6(0), B6(1), B6(1), B6(2) };
+const static uint8_t bit_totals[256] = { B6(0), B6(1), B6(1), B6(2) };
 #undef B6
 #undef B4
 #undef B2
