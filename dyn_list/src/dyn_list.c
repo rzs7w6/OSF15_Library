a703466f41267f3c316c917d2fe379527e7e3000
Will Starms <vilhelmen@gmail.com>
2015-10-15 19:49:06 -0500

    other stuff, RIP dyn_core

--- dyn_list/src/dyn_list.c
+++ dyn_list/src/dyn_list.c
@@ -13,23 +13,16 @@ struct dyn_list {
 
 // Since I can't scan my notes and attach it here...
 /*
-    going to have the front and back root pointers point to nodes, duh, but nodes MAY OR MAY NOT point back to root
-    because I'm not sure about implementation yet (should just require an extra loop condition)
-    (two days into tinkering with implementation seems to indicate that this is a bad idea)
-    (this is really only done in the STL to do a quick empty check (empty = front == back))
-    (but I have a size value, so having the ends link somewhere, especially to root, who has no data, is just weird)
+    Root connects to the two ends, BUT THE TWO ENDS ALSO CONNECT TO ROOT. It makes relinking faster, and NO LINK IS EVER NULL
+    Root, when empty, should point to itself at both ends. PREFER SIZE CHECK TO LINK CHECK
 
     There is a vital test in the test folder. It will assert that the relative offset of root's back and front are the same
-    as the node relative offsets. This way, root can be cast to a node and not cause a freakout, and then we can probably do the full
-    circular link.
-    Another vital assert will be that the size of the node is equal to the sum of it's parts. Padding will mess up addressing
+    as the node relative offsets. This way, root can be cast to a node and not cause a freakout, and that the circular link is fine
 
     Not going to store the size and destructor in the nodes because that's a major waste of space, though it is a little more restrictive
-    The list-who-shall-not-be-named will be interesting and do this to pull off... weird things.
+    The list-who-shall-not-be-named (hetero_list) will be interesting and allow varying size and destructors
+    (does that make this homo_list? It would be a better name than dyn_list...)
 */
-
-// Because nodes are getting weird to allow for contiguity
-#define NODE_MALLOC(data_size) malloc(sizeof(node_t) + data_size)
 
 // #define DATA_POINTER(node_ptr) ((void *)((&(node_ptr->next)) + 1))
 // WOAH, this is WAY better. This removes the requirement that the node size be the sum of the parts
@@ -37,17 +30,34 @@ struct dyn_list {
 // ...but isn't padding meant to be wasted?
 #define DATA_POINTER(node_ptr) (void *) ((node_ptr + 1)))
 
+// Does what it sounds like, inserts count objects from data_src at position
+// False on parameter or malloc failure
+bool dyn_core_insert(dyn_list_t *const dyn_list, const size_t position, const size_t count, const void *const data_src);
 
+// Extracts count objects from position, placing it in data_dest and removing the nodes
+// False on parameter error
+bool dyn_core_extract(dyn_list_t *const dyn_list, const size_t position, const size_t count, void *data_dest);
 
-// Modes of operation for dyn_shift
-// ...purge is more fun than remove. TODO: Grow up and rename PURGE to REMOVE... maybe
-typedef enum {INSERT = 0x01, EXTRACT = 0x02, PURGE = 0x04} DYN_CORE_MODE;
+// Deconstructs count objects at position and removes the nodes.
+// False on parameter error
+bool dyn_core_deconstruct(dyn_list_t *const dyn_list, const size_t position, const size_t count);
 
-// The heart of any non-trivial operation, check the impl for details
-// It'll validate data, just pass it the data with minimal checks
-bool dyn_core(dyn_list_t *const dyn_list, const size_t position, const size_t count, const DYN_CORE_MODE mode, void *const data_location);
+// Hunts down the requested node, NULL on parameter issue
+node_t *dyn_core_locate(const dyn_list_t *const dyn_list, const size_t position);
 
+/*
+    Considering hiding even dyn_core_locate down below, making EVERYTHING a thin wrapper to a dyn_core
+    But the complex functions (sort, prune, map/transform) would end up just being a wrapper of the dyn_core version
+        Which seems like a dumb level of indirection
 
+    So, for now, just call DATA_POINTER on the locate... ocne you've check locate didn't NULL on you
+        Maybe a locate_data would be better, even if it's just sticking the two together (inline!)
+
+    // Hunts down the data pointer at the requested node, NULL on parameter issue
+    void *dyn_core_locate_data(const dyn_list_t *const dyn_list, const size_t position);
+*/
+
+// This should be the only creation function
 dyn_list_t *dyn_list_create(const size_t data_type_size, void (*destruct_func)(void *const)) {
     dyn_list_t dyn_list = NULL;
     if (data_size) {
@@ -106,6 +116,7 @@ void dyn_list_destroy(dyn_list_t *const dyn_list) {
     }
 }
 
+
 //
 ///
 // HERE BE DRAGONS
@@ -113,76 +124,41 @@ void dyn_list_destroy(dyn_list_t *const dyn_list) {
 //
 
 
-/*
+// Because nodes are getting weird and I love them
+#define NODE_MALLOC(data_size) malloc(sizeof(node_t) + data_size)
 
-    OH BOY, DYN_SHIFT BUT FOR WEIRD DATA
+// Returns an array of linked nodes of size count
+// Remember to set the pref of the front and next of the back
+// Also, free the returned pointer when linked up
+// Null on param or malloc failure
+// Probably overkill if you're just creating one node
+node_t **dyn_core_allocate(const size_t count, const size_t data_size);
 
-    dyn_core is the, well, core of any non-trivial dyn function
-     (though traversal will probably need to be a function somewhere)
-    You want it? dyn_core does it... assuming you're editing N rectilinear nodes. Otherwise, go away
+// Declared up top since core is gone
+//bool dyn_core_insert(dyn_list_t *const dyn_list, const size_t position, const size_t count, const void *const data_src);
+//bool dyn_core_extract(dyn_list_t *const dyn_list, const size_t position, const size_t count, void *data_dest);
+//bool dyn_core_deconstruct(dyn_list_t *const dyn_list, const size_t position, const size_t count);
 
-    So, let's get started.
+// DO NOT TOUCH IF YOU ARE NOT CORE EXTRACT OR DECONSTRUCT
+// It will break all of your hopes, dreams, and data integrity if you use it wrong
+// It doesn't even check if these objects are all in the same list
+// This is meant to be a speedy helper
+// DO NOT TOUCH
+// YOU HAVE BEEN WARNED
 
-    Configuration is determined by the mode parameter. It's a DYN_CORE_MODE enum.
-    Right now there's INSERT, EXTRACT, and PURGE. They do what they sound like.
-
-    These operations are designed to be as safe as possible. If we return true, you're good to go.
-        If it's false, well, keeping a clean state may be difficult since we're not contiguous and can't just unwind
-        There will be a lot of error code :/ BUT! If it's false, the list will stay in the condition it was given
-        It might be a parameter thing. It might be that the list is totaly busted. You may be out of memory.
-        That's for you to figure out or just pass along. I miss exceptions. NO ERRNO AHHH.
-
-        Oh, unless we segfault. But we shouldn't. Does that mean we have to validate every pointer? Idk!
-        We might just validate root and assume the data didn't get mangled. That's potentially dangerous
-        since this is supposed to be for OS code and stuff... so we probably shouldn't do that. We'll see.
-
-    INSERT will attempt to insert N nodes before the given position, using data from the data array
-        Uhh... inserting on root may freak it out. Important non-edge edge case (surface case?), haha.
-        The doubly-linked nature in general may be a little annoying to manage and the usual edges may
-        become a major pain since we're managing EVERYTHING
-        If basic sanity checks pass, this operation may fail due to memory allocations.
-        We have to do a lot of them (N+1) and they may be large.
-
-    EXTRACT will attempt to extract the data from N nodes, startign with the given position, and
-        pipe the data into the given data location, deleting the nodes once it's finished.
-        This means it has to do two traversals. Deal.
-        If basic sanity checks pass, this operation never fails.
-
-    PURGE will, well, purge the data, applying the destructor if applicable. This may or may not be
-        two traversals. I'm still figuring that out.
-        Purge isn't exactly a "safe" operation in that we can't really revert if it breaks.
-        If basic sanity checks pass, this operation never fails.
-
-    HOO BOY, this funcion, with all its edge cases and whatnot is going to be WAY TOO BIG. Like, crazy complex.
-    Each mode had its own pile of concerns. dyn_shift was ok for dyn_array, but I think dyn_core is going to be too unmanageable
-    How sad :C
-
-*/
-/*
-    bool dyn_core(dyn_list_t *const dyn_list, const size_t position, const size_t count, const DYN_CORE_MODE mode, void *const data_location) {
-
-        if (dyn_list && count) {
-            if (mode == INSERT && position <= dyn_list->size) {
-
-
-            } else if (mode & (PURGE | EXTRACT)) { // we'e some sort of removal operation
-                if ((position + count) <= dyn_list->size) {
-                    // request is sane
-
-                }
-            }
-        }
-        return false;
+// ANYWAY, unlinks nodes begin and end from the list, correcting any and all links, decrements count,
+//  and then starts killing all nodes from begin to end
+void dyn_core_purge(dyn_list_t *const dyn_list, node_t *begin, node_t *end, const size_t count) {
+    if (dyn_list && begin && end && count) {
+        dyn_list->size -= count; // COUNT = CORRECTED, LIST IN BAD STATE
+        begin->prev->next = end->next; // BEGIN UNLINKED AND RE-ROUTED, LIST CANNOT BE BACK-TRAVERSED CORRECTLY, LIST IN BAD STATE
+        end->next->prev = begin->prev; // END UNLINKED AND RE-ROUTED, LIST IN GOOD STATE
     }
-*/
-bool dyn_core_insert(dyn_list_t *const dyn_list, const size_t position, const size_t count, const void *const data_src);
 
-bool dyn_core_extract(dyn_list_t *const dyn_list, const size_t position, const size_t count, void *data_dest);
+}
 
-bool dyn_core_purge(dyn_list_t *const dyn_list, const size_t position, const size_t count);
-
-// rename to at? Locate? ??????
-node_t *dyn_core_traverse(const dyn_list_t *const dyn_list, const size_t position) {
+// returns node pointer of requested node, NULL on bad request
+node_t *dyn_core_locate(const dyn_list_t *const dyn_list, const size_t position) {
     node_t *itr = NULL;
     if (dyn_list && position < dyn_list->size) {
         // Might be a bad idea
@@ -196,12 +172,12 @@ node_t *dyn_core_traverse(const dyn_list_t *const dyn_list, const size_t positio
         // In a perfect world where I have time to do things, I'd test which is better
         // Becuse that sounds neat, but that addition sounds hmmmm
         // Screw it, let's do something cool.
-        
+
         int offset = (position <= dyn_list->size >> 1) ? 1 : 0;
         itr = ((node_t **)dyn_list)[offset]; // itr is now either front or back node
-        for(size_t distance = offset ? position : size - position - 1;
-            distance;
-            --distance) {
+        for (size_t distance = offset ? position : size - position - 1;
+                distance;
+                --distance) {
             itr = ((node_t **)itr)[offset]; // So, 1 for forward, 0 for backwards. Neat, huh?
         }
 
